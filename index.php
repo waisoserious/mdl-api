9ed6094aaefa4b24a3149f48c78dad00918a7874
YoungN00bie <46465083+youngn000bie@users.noreply.github.com>
2020-11-07 10:44:09 +0700

    Add Years + Cast & Role

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST');
-// header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 if (isset($_GET['url']) && $_GET['url'] != '') {
 
   $html = file_get_contents($_GET['url']);
   // Get Info 
   preg_match("/film-title\">(.*?)>(.*?)<\/a>/si", $html, $title);
+  preg_match("/film-title\">(.*?)>(.*?)<\/h1>/si", $html, $years);
+  preg_match("/\((.*?)\)/si", $years[2], $year );
   preg_match("/<img class=\"img-responsive\" itempropx=\"image\" src=\"(.*?)\"/si", $html, $poster);
   preg_match("/class=\"inline\">Native Title:<\/b>(.*?)<\/li>/si", $html, $nativeAll);
   preg_match_all("/>(.*?)<\/a>/si", $nativeAll[1], $native);
@@ -30,11 +32,22 @@ if (isset($_GET['url']) && $_GET['url'] != '') {
   preg_match_all("/\">(.*?)<\/a>/si", $directorAll[1], $director);
   preg_match("/class=\"inline\">Screenwriter:<\/b>(.*?)<\/li>/si", $html, $screenwriterAll);
   preg_match_all("/\">(.*?)<\/a>/si", $screenwriterAll[1], $screenwriter);
-  preg_match("/<div class=\"p-a-sm\">  <ul class=\"list no-border p-b\">(.*?)<\/ul>/si", $html, $casts);
-  preg_match_all("/itempropx=\"name\">(.*?)<\/b>/si", $casts[1], $cast);
+  preg_match("/<ul class=\"list no-border p-b\">(.*?)<\/ul>/si", $html, $casts);
+  preg_match_all("/<li class=\"list-item col-sm-4\">(.*?)<\/li>/si", $casts[1], $cast);
   preg_match("/class=\"inline\">Tags:<\/b>(.*?)<\/li>/si", $html, $tagsAll);
   preg_match_all("/\">(.*?)<\/a>/si", $tagsAll[1], $tag);
   preg_match("/show-synopsis\">(.*?)>(.*?)<\/span>/si", $html, $syx);
+  
+  $result = [];
+  
+  foreach ($cast[1] as $castx) {
+    preg_match('/itempropx=\"name\">(.*?)<\/b>/si', $castx, $nama);
+    preg_match('/lass=\"text-muted\">(.*?)<\/small>/si', $castx, $role);
+    $result[] = [
+        'nama' => $nama[1],
+        'role' => $role[1],
+    ];
+  }
 
   $synopsis = str_replace(['<span class="read-more-hidden">', '<span>', '<p>'], ' ', $syx[2]);
   $tags = str_replace('(Vote or add tags)', ' ', $tag[1]);
@@ -44,6 +57,7 @@ if (isset($_GET['url']) && $_GET['url'] != '') {
 
   echo json_encode([
     'title'         => $title[2],
+    'years'         => $year[1],
     'poster'        => $poster[1],
     'native'        => $native[1],
     'alsoknown'     => $alsoknown[1],
@@ -58,43 +72,11 @@ if (isset($_GET['url']) && $_GET['url'] != '') {
     'duration'      => $duration[1],
     'director'      => $director[1],
     'screenwriter'  => $screenwriter[1],
-    'cast'          => $cast[1],
+    'cast'          => $result,
     'tags'          => $tags,
     'synopsis'      => $synopsis,
   ]);
 
   exit();
-} ?>
+} 
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MDL Info</title>
-  <!-- Bootstrap core CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-</head>
-
-<body>
-  <div class="my-4">
-    <div class="container">
-      <div class="card">
-      <div class="card-body">
-        <form method="GET" action="">
-          <div class="form-group">
-            <label for="url">Input MDL URL</label>
-            <input type="text" id="url" name="url" class="form-control" placeholder="https://mydramalist.com/49865-psycho-but-it-s-okay">
-          </div>
-          <button type="submit" class="btn btn-pr imary btn-block">Get Info</button>
-        </form>
-      </div>
-      </div>
-    </div>
-  </div>
-</body>
-
-</html>
